a07f6b912ff1bae0da0ea17e2fc96d2083578d22
Pascal Chambon <pythoniks@gmail.com>
2021-09-23 14:05:21 +0200

    Cleanup wa_keygen_gui.py

--- wa_keygen_gui.py
+++ wa_keygen_gui.py
@@ -3,100 +3,34 @@ import os
 
 os.environ["WACLIENT_TYPE"] = "APPLICATION"  # IMPORTANT
 from waguilib import kivy_presetup  # IMPORTANT
+del kivy_presetup
 
-import locale
 import sys
-from concurrent.futures.thread import ThreadPoolExecutor
-from functools import partial
-
-from kivy.animation import Animation
-from kivy.clock import Clock
-from kivy.core.window import Window
-from kivy.factory import Factory
-from kivy.metrics import dp
 from kivymd.app import MDApp
-from kivymd.uix.button import MDFlatButton
-from kivymd.uix.dialog import MDDialog
-from kivymd.uix.label import Label
-from kivymd.uix.list import IconLeftWidget
-from kivymd.uix.screen import Screen
-
-from wacryptolib.authentication_device import (
-    list_available_authentication_devices,
-    initialize_authentication_device,
-    _get_key_storage_folder_path, load_authentication_device_metadata,
-)
-from wacryptolib.key_generation import generate_asymmetric_keypair
-from wacryptolib.key_storage import FilesystemKeyStorage
-from wacryptolib.utilities import generate_uuid0
-
-from waguilib.i18n import Lang
-from waguilib.widgets.layout_helpers import activate_widget_debug_outline
+from waguilib.i18n import Lang, DEFAULT_LANGUAGE
 
 
-"""
-class A:  # TODO put back translation tooling
-    def _(self, a):
-        return a
-"""
-LOCALE_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), "locale")
-
+# IF NEEDED TO DEBUG
 #activate_widget_debug_outline()
 
-# VERY rough detection of user language, will often not work under Windows but it's OK
-_lang_code, _charset = locale.getlocale()
-DEFAULT_LANGUAGE = "fr" if "fr" in _lang_code.lower() else "en"
+
+LOCALE_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), "locale")
+
 
 class MainApp(MDApp):
-
 
     tr = Lang(DEFAULT_LANGUAGE, locale_dir=LOCALE_DIR)  # ("en")  # FIXME replace this with real trans
 
     kv_file = "wa_keygen_gui.kv"
-    keygen_panel = None
-
-    #authentication_device_list = ()
-    #authentication_device_selected = None
-
-    class COLORS:  # FIXME OBSOLETE
-        LIGHT_GREY = [1, 1, 1, 0.4]
-        MEDIUM_GREY = [0.6, 0.6, 0.6, 1]
-        DARK_GREY = [0.3, 0.3, 0.3, 0.4]
-        DARKEST_BLUE = [0.1372, 0.2862, 0.5294, 1]
-        DARK_BLUE = [0.1272, 0.2662, 0.4294, 1]
-        BUTTON_BACKGROUND = [51, 23, 186, 1]  # Not same format as others, as its' a tint
-        WHITE = [1, 1, 1, 1]
 
     def __init__(self, **kwargs):
         self.title = "Witness Angel - Guardian Authenticator"
         super(MainApp, self).__init__(**kwargs)
 
-    @property
-    def screen_manager(self):
-        if not self.root: return  # Early introspection
-        return self.root
-
-    @property
-    def ___keyring_selector_screen(self):
-        """Beware, here we lookup not the config file but the in-GUI data!"""
-        if not self.root:
-            return  # Early introspection
-        return self.screen_manager.get_screen("keyring_selector_screen")
-
     def build(self):
-
         self.theme_cls.primary_palette = "Blue"
         #self.theme_cls.theme_style = "Dark"  # or "Light"
         self.theme_cls.primary_hue = "900"  # "500"
-
-        # FIXME - DUPLICATED WITH WAGUILIB NOW
-
-
-        #self.authentication_device_selected = None
-        #self.orientation = "vertical"
-        #self.screen = Screen()
-        #self.list_detected_devices()
-        #return self.screen
 
 
 def resourcePath():
