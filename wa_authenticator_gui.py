c5e702a1fb702d810c6b48ec9bb86548ba63f79b
Pascal Chambon <pythoniks@gmail.com>
2021-09-24 14:49:03 +0200

    Fix PyInstaller build system

--- wa_authenticator_gui.py
+++ wa_authenticator_gui.py
@@ -1,3 +1,6 @@
+
+print(">>>> WE LIVE IN", __file__)
+
 import os
 
 os.environ["WACLIENT_TYPE"] = "APPLICATION"  # IMPORTANT
@@ -7,7 +10,7 @@ del kivy_presetup
 from pathlib import Path
 
 from kivymd.app import MDApp
-from kivy.resources import resource_find
+from kivy.resources import resource_find, resource_add_path
 
 from waguilib.i18n import tr
 from waguilib.locale import LOCALE_DIR as GUILIB_LOCALE_DIR  # DEFAULT LOCALE
@@ -22,14 +25,15 @@ ROOT_DIR = Path(__file__).parent
 LOCALE_DIR = ROOT_DIR / "locale"
 tr.add_locale_dirs(LOCALE_DIR, GUILIB_LOCALE_DIR)
 
+resource_add_path(ROOT_DIR)
 
 class WaAuthenticatorApp(MDApp):
 
-    kv_file = "wa_authenticator_gui.kv"
+    kv_file = resource_find("wa_authenticator_gui.kv")
     icon = resource_find("icons/witness_angel_logo_blue_32x32.png")
 
     def __init__(self, **kwargs):
-        self.title = "Witness Angel - Guardian Authenticator"  # Untranslated
+        self.title = "Witness Angel - Authenticator Manager"  # Untranslated
         super().__init__(**kwargs)
 
     def build(self):
