9a9324aebc59db690a4ddff11bc1eb9249cb7b3e
Pascal Chambon <pythoniks@gmail.com>
2022-01-24 13:30:55 +0100

    Add settings panel to keygen app

--- wa_authenticator_gui.py
+++ wa_authenticator_gui.py
@@ -33,13 +33,32 @@ resource_add_path(ROOT_DIR)
 
 class WaAuthenticatorApp(WaGenericGui):
 
-    title = "Witness Angel - Authenticator Manager"  # Untranslated
+    title_app_window = "Witness Angel - Authenticator Manager"  # Untranslated
+    title_conf_panel = tr._("Keygen settings")
+
     kv_file = resource_find("wa_authenticator_gui.kv")
     icon = resource_find("icons/witness_angel_logo_blue_32x32.png")
+
+    config_file_basename = "wakeygen_config.ini"
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         Window.bind(on_keyboard=self.handle_back_button)
+
+    def on_start(self):
+        super().on_start()
+        main_screen = self.root.ids.screen_manager.get_screen(
+                    "authenticator_selector_screen"
+                )
+        assert hasattr(main_screen, "selected_custom_folder_path")
+        main_screen.selected_custom_folder_path = self.get_custom_authenticator_dir()
+
+        main_screen.bind(selected_custom_folder_path=self._handle_selected_custom_folder_path_changed)
+
+    def _handle_selected_custom_folder_path_changed(self, source, custom_authenticator_dir):
+        """Persist any "currently selected custom folder" to config file"""
+        self.config["keygen"]["custom_authenticator_dir"] = str(custom_authenticator_dir)
+        self.save_config()
 
     def handle_back_button(self, widget, key, *args):
 
@@ -56,3 +75,10 @@ class WaAuthenticatorApp(WaGenericGui):
                 return True
 
             # Else, let the key propagate (and app close if necessary)
+
+    def get_witness_angel_gateway_url(self):
+        return self.config.get("keygen", "witness_angel_gateway_url")
+
+    def get_custom_authenticator_dir(self):
+        raw_value = self.config.get("keygen", "custom_authenticator_dir").strip()
+        return Path(raw_value).absolute() if raw_value else None  # Beware, "" must not be interpreted as "./"
