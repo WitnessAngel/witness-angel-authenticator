91bae466bb8a0aba464862db70aabaf2cecb9087
Pascal Chambon <pythoniks@gmail.com>
2022-03-01 12:58:50 +0100

    Add crashdump handler to keygen app

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from wa_authenticator_gui import WaAuthenticatorApp
 
 if __name__ == "__main__":
-    WaAuthenticatorApp().run()
+    from wacomponents.launcher import launch_app_or_service_with_crash_handler
+    launch_app_or_service_with_crash_handler("wa_authenticator_gui", client_type="APPLICATION")
