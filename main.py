79f19f5b83c55ca065a069a2904d3bc64b53bef8
Pascal Chambon <pythoniks@gmail.com>
2023-08-22 09:43:59 +0200

    Display python version on launch

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import sys
 from pathlib import Path
 
+print(">>>> PYTHON VERSION:", sys.version)
+
+
 if __name__ == "__main__":
     from wacomponents.launcher import launch_main_module_with_crash_handler
     launch_main_module_with_crash_handler("waauthenticator.waauthenticator_gui", client_type="APPLICATION")
